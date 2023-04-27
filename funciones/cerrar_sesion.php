1f0d1284b9085660ba3552fedad9af55c33e7cf7
Cesar Leonardo Insuasty Moreno <40036889+cinsuasty@users.noreply.github.com>
2023-04-27 18:28:22 -0500

    Primer commit, inicio proyecto

---
+++ funciones/cerrar_sesion.php
@@ -0,0 +1,9 @@
+<?php
+        session_start();
+        session_destroy();
+        echo '<script language="javascript">
+                alert("Usted, ha cerrado sesión.");
+                sessionStorage.clear();
+                window.location="/../../index.html "
+            </script>';
+?>
