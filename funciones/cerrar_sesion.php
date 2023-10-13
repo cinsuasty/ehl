dd485d67c40e8fbd2a62df3aeb404fe6a9df409d
Cesar Leonardo Insuasty Moreno <40036889+cinsuasty@users.noreply.github.com>
2023-10-13 11:54:57 -0500

    delete functions

--- funciones/cerrar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-        session_start();
-        session_destroy();
-        echo '<script language="javascript">
-                alert("Usted, ha cerrado sesión.");
-                sessionStorage.clear();
-                window.location="/../../index.html "
-            </script>';
-?>
